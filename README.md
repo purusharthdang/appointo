95b8e7f561550c4a9b6c20fe5b3a41f51bbbe45f
Purusharth Dang <dang-pc@Purusharths-MacBook-Air.local>
2024-01-28 21:31:28 +0530

    all functionality and UI updates

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
-# Getting Started with Create React App
+# SidePanda
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+Appointo is an app that helps users seamlessly scehdule appointments!
+
+1. Select date range.
+2. Select available time slot.
+3. Click on Next!
 
 ## Available Scripts
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Appointments from './pages/Appointments';
+import { Provider } from 'react-redux';
+import { store } from './connections/store';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <Appointments />
+    </Provider>
   </React.StrictMode>
 );
 
